0bf040b285baf3bc21e12591147db7725faf2ee3
Rik Smith-Unna <rik@fathomlabs.io>
2019-07-10 13:56:04 +0100

    Check for Leaflet, require if needed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-
+if (!globalThis.L) {
+  try {
+    globalThis.L = require('leaflet')
+  } catch (e) {
+    throw new Error('Leaflet is not available')
+  }
+}
 export { MarkerClusterGroup } from './MarkerClusterGroup.js';
 export { MarkerCluster } from './MarkerCluster.js';
 import {} from './MarkerOpacity.js';
